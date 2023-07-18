160f4465888c4b0ee8c47ba2b44b4bd91e065331
SMFaysalHaque <smfaysalhaque007@gmail.com>
2023-07-18 18:48:11 +0600

    first commit!

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'W1vLyxT5rQ15jBpANjnv0gtt',
+        apiOptions: {
+           region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ],
+  vite: {
+    optimizeDeps: { exclude: ["fsevents"] }
+  }
+})
+
